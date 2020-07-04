fc51fd49f44b1275d097c9808965700b6497dd56
Brittney <brittney.postma@hotmail.com>
2020-07-04 18:04:01 -0400

    work on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,23 @@
 import React from "react"
+import { Link } from 'gatsby'
 import logo from "../images/logo.png"
+import cart from "../images/cart.png"
 
-const Header = ({ siteTitle }) => (
-  <header>
+const badge = `absolute -mt-12 -mr-3 right-0 bg-white text-orange border-red border-solid border-2 rounded-full h-6 w-6 leading-5 pl-1`
+
+const Header = () => (
+  <header className="flex items-center justify-between bg-red px-10 py-4">
     <img src={logo} alt="Keiko Ramen logo" />
-    <h1>Header</h1>
+    <nav className="flex items-center">
+      <Link to='/menu'>MENU</Link>
+      <Link to='/location'>LOCATION</Link>
+      <Link to='/cart'>
+        <div className="relative"><img src={cart} alt="shopping cart" /><p className={badge}>0</p></div>
+      </Link>
+      <Link to='/order'>
+        <button className="bg-orange rounded-full ml-5 p-3">ORDER NOW</button>
+      </Link>
+    </nav>
   </header>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ["./src/styles/main.css", "./src/**/*.js", "./src/**/*.jsx"],
   theme: {
     //! anything here overrides tailwinds defaults //
+    colors: {
+      white: "#efefef",
+      black: "#111111",
+      red: "#A50022",
+      darkRed: "#640A13",
+      orange: "#FF7800"
+    },
     fontFamily: {
       heading: "'Merienda One', cursive",
       body: "'Advent Pro', sans-serif",
