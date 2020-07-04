8b889e1b02ba1d60fbc3bddd9d6d1d2c58761b8a
Brittney <brittney.postma@hotmail.com>
2020-07-03 21:16:46 -0400

    tailwind installation

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@brittneypostma`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
