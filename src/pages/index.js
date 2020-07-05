a712239e37e0c29f382cbcd6ddad761e1664c09c
Brittney <brittney.postma@hotmail.com>
2020-07-05 12:01:46 -0400

    scrollbars

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <img src={bg} alt="bowl of ramen with chopsticks" className="absolute top-0 w-screen" />
-    <h1 className="center top-150 text-white">Keiko Ramen</h1>
+    <h1 className="center top-150 text text-white text-shadow text-4xl lg:text-6xl">Keiko Ramen</h1>
 
   </Layout>
 )
