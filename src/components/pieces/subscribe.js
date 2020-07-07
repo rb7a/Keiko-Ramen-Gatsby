d7aa23e755ab4e4642633795afb0b51c4a097e27
Brittney <brittney.postma@hotmail.com>
2020-07-06 23:40:22 -0400

    form fix and margin max on xl for carousel

--- src/components/pieces/subscribe.js
+++ src/components/pieces/subscribe.js
@@ -7,10 +7,13 @@ const Subscribe = () => {
         <img src={subscribe} alt="subscribe for deals and offers" className="hidden lg:block" />
         <h3 className="lg:hidden text-white">Subscribe for<br />deals & offers!</h3>
         <div className="grid justify-center lg:absolute email">
-          <label for="email" className="text-white text-xl">Enter your email</label><br />
+
           <div className="flex">
-            <input type="email" id="email" name="email" />
-            <div className="bg-orange p-2">
+            <div className="grid">
+              <label htmlFor="email" className="text-white text-xl">Enter your email<br />
+                <input type="email" id="email" name="email" /></label>
+            </div>
+            <div className="bg-orange p-2 self-end">
               <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M8.99718 11.5941L9.72467 12.41L10.4728 11.6129L19 2.52683V17H1V2.62429L8.99718 11.5941ZM2.23131 1H17.6901L9.76251  1Z" stroke="white" stroke-width="2" />
               </svg>
--- src/components/sections/carousel.js
+++ src/components/sections/carousel.js
@@ -6,7 +6,7 @@ import beef from '../../images/full-images/beef-ramen.jpg'
 const Carousel = () => {
   return (
     <div className="px-0 py-5 sm:p-10">
-      <div className="p-10 flex justify-between gap-5 shadow-xl">
+      <div className="p-10 flex justify-between gap-5 shadow-xl xl:mx-48">
         <div className="w-full xl:w-1/2">
           <h2>Spicy Beef<br />Ramen</h2>
           <p>Bean sprouts bamboo slices Tokyo kamaboko Kumamoto toasted sesame seeds Nagoya leek bean sprouts scallions leek minced garlic curry bean sprouts ginger. Yokohama soy milk soy milk seasoned egg ground black pepper chopped onions roasted pork slices spinach seasoned egg kamaboko chicken stock pork bones sesame oil chicken stock Hakata minced garlic Wakayama ramen burger toasted sesame seeds. Abura soba abura soba butter Hakata ramen burger Hakata Kumamoto tsukemen curry Asahikawa. Soy milk Kagoshima yuzu butter salt Nissin instant cup ramen Wakayama soy sauce chicken stock Hakata toasted sesame seeds ramen burger flavoured oil. Seasoned egg Tokushima Sapporo butter ramen burger minced garlic Tokyo wood ear mushroom soy milk bamboo slices Asahikawa Nagoya pork bones Kagoshima leek vinegar Kagoshima Tokyo leek. Kagoshima lard Asahikawa mustard greens Sapporo yuzu fish broth wood ear mushroom Wakayama tsukemen soy milk yuzu nori yuzu curry Tokyo bean sprouts Kagoshima corn. Soy sauce pork bones fish broth Tokushima chilli vinegar vinegar nori vinegar soy sauce.</p>
