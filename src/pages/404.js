e4b14b2c8ff46f37866cab8d2a96b7047daf9791
Brittney <brittney.postma@hotmail.com>
2020-07-07 20:51:07 -0400

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react"
-
+import { Link } from 'gatsby'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="relative bg-clear-top grid gap-5 text-center items-center content-center mt-24">
+      <h1 className='text-red'>NOT FOUND</h1>
+      <p className="text-xl tracking-widest">You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to='/'><button className="hover:btn-hover border-orange border-2 hover:border-solid ">Go get more ramen</button></Link>
+    </div>
   </Layout>
 )
 
