1091f64544993d60cbd9caf07fb2263e505df769
Brittney <brittney.postma@hotmail.com>
2020-07-15 19:17:15 -0400

    styling done, I hope

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { Link } from 'gatsby'
 import logo from "../../images/logo.png"
 import cart from "../../images/cart.png"
@@ -13,9 +13,11 @@ const menuStyles = `mobile-menu rounded-md border-solid border-black border-4 lg
 const linksStyles = `border-b-0 border-solid border-orange hover:border-b-4 `
 const badgeStyles = `absolute -mt-12 -mr-2 right-0 bg-white text-orange border-red border-solid border-2 rounded-full h-6 w-6 leading-5 pl-1`
 
-const Header = ({ sticky }) => {
-  const [navbarOpen, setNavbarOpen] = useState(false)
+const Header = () => {
+  const ref = useRef();
+  const [navOpen, setNavOpen] = useState(false)
   const [isTop, setIsTop] = useState(true)
+  useOnClickOutside(ref, () => setNavOpen(false))
 
   useEffect(() => {
     document.addEventListener('scroll', () => {
@@ -29,6 +31,7 @@ const Header = ({ sticky }) => {
     });
   }, [])
 
+
   return (
     <header className={isTop ? headerStyles : stickyHeader}>
       <div className={headerWrapper}>
@@ -39,7 +42,7 @@ const Header = ({ sticky }) => {
           <button
             className={isTop ? mobileNavBtn : navBtnScroll}
             aria-label="menu"
-            onClick={() => setNavbarOpen(!navbarOpen)}
+            onClick={() => setNavOpen(true)}
           >
             <div>
               <span className={hamburgerMenu}></span>
@@ -48,23 +51,24 @@ const Header = ({ sticky }) => {
             </div>
           </button>
           <nav
+            ref={ref}
             className={
               "absolute lg:flex flex-grow items-center" +
-              (navbarOpen ? "grid" : " hidden")
+              (navOpen ? "grid" : " hidden")
             }
           >
             <ul className={menuStyles}>
               <li>
-                <Link to='/' className={linksStyles}>HOME</Link>
+                <Link to='/' className={linksStyles} onClick={() => setNavOpen(false)}>HOME</Link>
               </li>
               <li>
-                <Link to='/menu' className={linksStyles}>MENU</Link>
+                <Link to='/menu' className={linksStyles} onClick={() => setNavOpen(false)}>MENU</Link>
               </li>
               <li>
-                <Link to='/location' className={linksStyles}>LOCATION</Link>
+                <Link to='/location' className={linksStyles} onClick={() => setNavOpen(false)}>LOCATION</Link>
               </li>
               <li>
-                <Link to='/order-now'>
+                <Link to='/order-now' onClick={() => setNavOpen(false)}>
                   <button className="ml-1 mr-2 hover:btn-hover">ORDER NOW</button>
                 </Link>
               </li>
@@ -81,3 +85,21 @@ const Header = ({ sticky }) => {
 }
 
 export default Header
+
+function useOnClickOutside(ref, handler) {
+  useEffect(() => {
+    const listener = event => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]
+  )
+}
--- src/components/menu/menu-item.js
+++ src/components/menu/menu-item.js
@@ -4,7 +4,7 @@ import cart from "../../images/cart.png"
 
 const itemWrapper = `rounded-sm bg-white shadow-inset sm:hover:shadow-none flex justify-between items-center pl-5 gap-5 w-full transition-transform ease-linear duration-75 transform sm:hover:scale-105 sm:hover:bg-orange sm:hover:text-white`
 
-const textWrapper = `w-full px-2 py-5 grid justify-center items-center md:flex`
+const textWrapper = `w-full px-2 py-5 grid justify-between items-center md:flex`
 
 const cartLink = `rounded-sm bg-orange self-stretch flex flex-col items-center justify-center p-5`
 
@@ -14,7 +14,7 @@ const MenuItem = ({ name, desc, price }) => {
   return (
     <div className={itemWrapper}>
       <div className={textWrapper}>
-        <h4 className="text-2xl w-9/12">{name}</h4>
+        <h4 className="text-2xl flex-1">{name}</h4>
         <p className="md:mx-5">{desc}</p>
         <h4 className="text-2xl">${price}</h4>
       </div>
